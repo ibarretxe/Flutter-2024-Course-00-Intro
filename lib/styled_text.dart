17ae7a9acec1bb356059408a4cc96e27b5787b95
Ibarretxe <cesar@ibarretxeguren@gmail.com>
2024-09-05 11:51:30 +0200

    Texto Widget Indepentiente. Ahora SI.

---
+++ lib/styled_text.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  @override
+  Widget build(context) {
+  return const Text(
+              'Hola Mundo!',
+              style: TextStyle(color: Colors.white, fontSize: 27),
+            );
+  }
+}
