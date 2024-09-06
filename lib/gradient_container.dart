7f39a6c2d537882eb5d5a2fc72cd4b2c1e205e01
Ibarretxe <cesar@ibarretxeguren@gmail.com>
2024-09-06 10:17:05 +0200

    Primera Tirada

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,26 +1,29 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer (this.color1, this.color2, {super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
   final Color color1;
   final Color color2;
+
+
   @override
   Widget build(context) {
     return Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [color1, color2],
-              begin: startAlignment,
-              end: endAlignment,
-            ),
-          ),
-          child: Center(
-            child: Image.asset('assets/images/dice-2.png'),
-            ),
-          );
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller()
+      ),
+    );
   }
 }
 // class GradientContainer extends StatelessWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(Color.fromARGB(255, 97, 52, 175), Color.fromARGB(255, 199, 25, 49)),
+        body: GradientContainer(
+          Color.fromARGB(255, 97, 52, 175), 
+          Color.fromARGB(255, 199, 25, 49)),
       ),
     ),
   );
